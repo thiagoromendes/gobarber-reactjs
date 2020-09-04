3c6648692f2b222310da02760465751cb35beb8a
Thiago Mendes <thiagoromendes@gmail.com>
2020-09-04 18:32:58 -0300

    Implementado a página de recuperação de senha e configurado sua rota

--- src/pages/ForgotPassword/index.tsx
+++ src/pages/ForgotPassword/index.tsx
@@ -1,13 +1,11 @@
 import React, { useCallback, useRef } from 'react';
-import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { FiLogIn, FiMail } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-import api from '../../services/api';
 import { useToast } from '../../hooks/Tost';
-
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input/index';
@@ -17,42 +15,33 @@ import logoImg from '../../assets/logo.svg';
 
 import { Container, Content, AnimationContainer, Background } from './styles';
 
-interface SignUpFormData {
-  name: string;
+interface forgotPasswordFormData {
   email: string;
-  password: string;
 }
 
-const SignUn: React.FC = () => {
+const ForgotPassword: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+
   const { addToast } = useToast();
-  const history = useHistory();
+  // const history = useHistory();
 
   const handleSubmit = useCallback(
-    async (data: SignUpFormData) => {
+    async (data: forgotPasswordFormData) => {
       try {
         formRef.current?.setErrors({});
         const schema = Yup.object().shape({
-          name: Yup.string().required('Nome é obrigatório'),
           email: Yup.string()
-            .required('E-mail é obrigatório')
+            .required('E-mail obrigatório')
             .email('Digite um e-mail válido'),
-          password: Yup.string().min(6, 'No mínimo 6 dígitos'),
         });
 
         await schema.validate(data, {
           abortEarly: false,
         });
 
-        await api.post('/users', data);
+        // Recuperação de senha
 
-        addToast({
-          type: 'success',
-          title: 'Cadastro realizado!',
-          description: 'Você já pode fazer seu logon no GoBarber!',
-        });
-
-        history.push('/');
+        // history.push('/dashboard');
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
           const errors = getValidationErrors(err);
@@ -60,41 +49,35 @@ const SignUn: React.FC = () => {
         } else {
           addToast({
             type: 'error',
-            title: 'Erro no cadastro',
-            description:
-              'Ocorreu um erro ao fazer o cadastro, cheque seus dados',
+            title: 'Erro na recuperação de senha',
+            description: 'Ocorreu um erro ao fazer a recuperação de senha',
           });
         }
       }
     },
-    [addToast, history],
+    [addToast],
   );
+
   return (
     <Container>
-      <Background />
       <Content>
         <AnimationContainer>
           <img src={logoImg} alt="GoBarber" />
           <Form ref={formRef} onSubmit={handleSubmit}>
-            <h1>Faça seu cadastro</h1>
-            <Input name="name" icon={FiUser} placeholder="Nome" />
+            <h1>Recuperar senha</h1>
             <Input name="email" icon={FiMail} placeholder="E-mail" />
-            <Input
-              name="password"
-              icon={FiLock}
-              type="password"
-              placeholder="Senha"
-            />
-            <Button type="submit">Cadastrar</Button>
+
+            <Button type="submit">Recuperar</Button>
           </Form>
           <Link to="/">
-            <FiArrowLeft />
-            Voltar para logon
+            <FiLogIn />
+            Voltar ao login
           </Link>
         </AnimationContainer>
       </Content>
+      <Background />
     </Container>
   );
 };
 
-export default SignUn;
+export default ForgotPassword;
--- src/routes/indedx.tsx
+++ src/routes/indedx.tsx
@@ -5,12 +5,15 @@ import Route from './Route';
 
 import Signin from '../pages/Sigin';
 import Signup from '../pages/Sigup';
+import ForgotPassword from '../pages/ForgotPassword';
 import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Signin} />
     <Route path="/signup" component={Signup} />
+    <Route path="/forgot-password" component={ForgotPassword} />
+
     <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
