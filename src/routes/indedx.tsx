5c1c0303e868e8d0262e7e0a946792c3ced366cc
Thiago Mendes <thiagoromendes@gmail.com>
2020-09-15 21:28:57 -0300

    Implementado página de edição de usuário

--- src/routes/indedx.tsx
+++ src/routes/indedx.tsx
@@ -7,6 +7,8 @@ import Signin from '../pages/Sigin';
 import Signup from '../pages/Sigup';
 import ForgotPassword from '../pages/ForgotPassword';
 import ResetPassword from '../pages/ResetPassword';
+
+import Profile from '../pages/Profile';
 import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
@@ -16,6 +18,7 @@ const Routes: React.FC = () => (
     <Route path="/forgot-password" component={ForgotPassword} />
     <Route path="/reset-password" component={ResetPassword} />
 
+    <Route path="/profile" component={Profile} isPrivate />
     <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
